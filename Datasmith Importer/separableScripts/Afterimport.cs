51f0cac9e8acc60a03ff833e0b181ded6c3c416a
unknown <alexlaconca@gmail.com>
2022-02-05 17:31:31 +0100

    print mesh size

--- Datasmith Importer/separableScripts/Afterimport.cs
+++ Datasmith Importer/separableScripts/Afterimport.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -9,25 +10,34 @@ public class Afterimport : MonoBehaviour
 {
 
 
+
     /// <summary>
     /// This method is called after the model is imported
     /// </summary>
     /// <param name="obj">The imported model</param>
     public static void afterImport(GameObject obj)
     {
-        Debug.Log("Import Complete");
+        List<Mesh> mr = obj.GetComponentsInChildren<MeshFilter>().Select(e => e.sharedMesh).ToList();
+
+        int nVerts = mr.Select(m => m.vertexCount).Sum();
+        int nTris = mr.Select(m => m.triangles.Length).Sum();
+        Debug.Log("Import Complete, \ntotal v    =" + nVerts + "\ntotal tris =" + nTris);
+
+        //obj.GetComponentsInChildren<MetadataManager>().ToList().ForEach(e => e.enabled = false);
+
+        //obj.GetComponentsInChildren<MeshCollider>().ToList().ForEach(e => e.enabled = false);
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
